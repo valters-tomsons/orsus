5d3e89380cafae40930cfc9d4e957be38ff7bff6
Valters <valters.tomsons@protonmail.com>
2019-12-30 00:42:01 +0200

    Introduce TextureCube scene

---
+++ src/orsus-engine/Scenes/TextureCube.cs
@@ -0,0 +1,27 @@
+using orsus_engine.Interfaces;
+using Veldrid;
+
+namespace orsus_engine.Scenes
+{
+    public class TextureCube : IOrsusScene
+    {
+        public TextureCube()
+        {
+        }
+
+        public void CreateResources(GraphicsDevice device)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DisposeResources()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Draw(GraphicsDevice device)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
